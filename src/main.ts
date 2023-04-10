6a342d3872ab8c1a6c58d475f9445a69bb736c8c
olaoluwa vincent <olaoluwavincent@gmail.com>
2023-04-10 07:16:06 +0100

    restarting dev server

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { ValidationPipe } from "@nestjs/common/pipes";
 import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
